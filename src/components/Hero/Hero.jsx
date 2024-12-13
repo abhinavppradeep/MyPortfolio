041155e91eb7d264053a0c5c25a8f58097f411ec
abhinavppradeep <abhinavppradeep2@gmail.com>
2024-12-13 21:33:15 +0530

    completed till hero part

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import heroImage from "../../../asset/hero/heroImage.png";
+import styles from "./Hero.module.css";
 
 export const Hero = () => {
   return (
-    <section>
-      <div>
-        <h1>Hi, I'm Abhinav</h1>
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Abhinav</h1>
+        <p className={styles.description}>
+          I’m a third-year ECE student passionate about AI, web development, and
+          app development. Reach out if you'd like to learn more!
+        </p>
+        <a
+          href="mailto:abhinavppradeep01@gmail.com"
+          className={styles.contactBtn}
+        >
+          Contact Me
+        </a>
       </div>
+      <img src={heroImage} alt="Hero image of me" className={styles.heroImg} />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
     </section>
   );
 };
