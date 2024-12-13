2a372b394579cdebed52ee21bb42c70a9067a90e
abhinavppradeep <abhinavppradeep2@gmail.com>
2024-12-13 12:39:53 +0530

    image not rendering

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,30 @@
-import React from 'react';
-import styles from './Navbar.module.css';
- 
+import React, { useState } from "react";
+import styles from "./Navbar.module.css";
+import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
   return (
-    <nav className={styles.navbar}> 
-        <a className={styles.title } href="/">Portfolio
-        <div className={styles.menu }>
-            <ul className={styles.menuitems }>
-                 <li>
-                     <a href='#about'>About</a>
-                 </li>
-                 <li>
-                     <a href='#expirience'>Expirience</a>
-                 </li>
-                 <li>
-                     <a href='#projects'>Projects </a>
-                 </li>
-                 <li>
-                     <a href='#contact'>Contact  </a>
-                 </li>
-            </ul>
-        </div>
-        </a>
+    <nav className={styles.navbar}>
+      <img className={styles.menuBtn} src={getImageUrl("nav/menuIcon.png")} />
+      <a className={styles.title} href="/">
+        Portfolio
+      </a>
+      <div className={styles.menu}>
+        <ul className={styles.menuItems}>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#expirience">Expirience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects </a>
+          </li>
+          <li>
+            <a href="#contact">Contact </a>
+          </li>
+        </ul>
+      </div>
     </nav>
-  )
-}
+  );
+};
